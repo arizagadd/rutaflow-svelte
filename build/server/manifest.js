039b1739bb3984a4e3f4c4471fc204324442dac4
Sam Gutiérrez <ksgutierrezb@gmail.com>
2024-06-26 10:16:02 -0600

    update comments that are shown to the driver

--- build/server/manifest.js
+++ build/server/manifest.js
@@ -10,13 +10,13 @@ return {
 	assets: new Set(["favicon.png","rutaflow.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.ChLDgHW6.js","app":"_app/immutable/entry/app.DAjWHLeN.js","imports":["_app/immutable/entry/start.ChLDgHW6.js","_app/immutable/chunks/entry.DuIQnMQH.js","_app/immutable/chunks/index.BYLndrM5.js","_app/immutable/entry/app.DAjWHLeN.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js","_app/immutable/chunks/entry.DuIQnMQH.js","_app/immutable/chunks/index.BYLndrM5.js","_app/immutable/chunks/index.BNHbygFo.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.DLXa4KCi.js","app":"_app/immutable/entry/app.9ZhZrtLT.js","imports":["_app/immutable/entry/start.DLXa4KCi.js","_app/immutable/chunks/entry.BwhmJIPv.js","_app/immutable/chunks/index.BYLndrM5.js","_app/immutable/entry/app.9ZhZrtLT.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js","_app/immutable/chunks/entry.BwhmJIPv.js","_app/immutable/chunks/index.BYLndrM5.js","_app/immutable/chunks/index.BNHbygFo.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./chunks/0-C3wrTScA.js')),
 			__memo(() => import('./chunks/1-BAn-v2h6.js')),
-			__memo(() => import('./chunks/2-CZ69T811.js')),
-			__memo(() => import('./chunks/4-Dw9yg9VX.js')),
-			__memo(() => import('./chunks/5-CRHyjNl3.js'))
+			__memo(() => import('./chunks/2-j5gqUGRh.js')),
+			__memo(() => import('./chunks/4-CWmVHSuB.js')),
+			__memo(() => import('./chunks/5-SZNKEq2a.js'))
 		],
 		routes: [
 			{
